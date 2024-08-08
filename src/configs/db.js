07ea80e424828f48add2ee01cd7db2b1ebb9d52e
Muhamad Rifki Hilman <51555828+rifkyhilman@users.noreply.github.com>
2024-08-09 05:59:20 +0700

    change db

--- src/configs/db.js
+++ src/configs/db.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: 'sql12.freesqldatabase.com',
     user: 'sql12724718',
     password: '6Hks9f12iE',
-    database: 'shuff'
+    database: 'sql12724718'
 });
 
 module.exports = {connection}
